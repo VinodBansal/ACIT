691e9263ce936bf97ade80a24bcb9e4b133c61af
VinodBansal <54194786+VinodBansal@users.noreply.github.com>
2019-08-31 20:20:59 +1000

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "Region for the VPC"
+  default = "ap-southeast-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR for the VPC"
+  default = "10.1.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.1.1.0/24"
+}
+
+variable "public_subnet2_cidr" {
+  description = "CIDR for the public subnet"
+  default = "10.1.2.0/24"
+}
+
+variable "ami" {
+  description = "Amazon Linux AMI"
+  default = "ami-0975ce566eec139c3"
+}
+
+variable "key_path" {
+  description = "SSH Public Key path"
+  default = "Public_Key"
+}
