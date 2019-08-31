976f197dc0fd25227598732d4dd51993956dbabf
VinodBansal <54194786+VinodBansal@users.noreply.github.com>
2019-08-31 20:19:36 +1000

    Create userdata.sh

---
+++ terraform/userdata.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+# output log of userdata to /var/log/user-data.log
+yum update
+yum install java-1.8.0-openjdk -y
+var=$(readlink -f $(which java))
+export JAVA_HOME=$var
+
+sudo yum install python3 -y
+sudo yum upgrade python3
+pip3 install ansible --user
+sudo yum install git-all
+sudo yum intstall wget zip unzip
