d5092b97773bf61c646e8d7b61335435fe6546a5
VinodBansal <54194786+VinodBansal@users.noreply.github.com>
2019-09-01 19:59:34 +1000

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
-set -x
+#!/bin/bash
 # output log of userdata to /var/log/user-data.log
+log=/home/boot_log.txt
 yum update
-yum install java-1.8.0-openjdk -y
+echo yum install java-1.8.0-openjdk -y >>$log
 var=$(readlink -f $(which java))
 export JAVA_HOME=$var
-sudo yum install python3 -y
-sudo yum install puppet
-sudo yum install git-all
-sudo yum install wget
-sudo yum install zip
+sudo yum install python3 -y >>$log
+sudo rpm -ivh https://yum.puppetlabs.com/puppet-release-el-8.noarch.rpm >>$log
+sudo yum install puppet >>$log
+sudo yum install wget >>$log
+sudo yum install zip >>$log
+sudo yum install git-all >>$log
