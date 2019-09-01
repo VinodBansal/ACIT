2eeade8b1c3d5c95e7ea85c6384aeb5e7f8af749
VinodBansal <54194786+VinodBansal@users.noreply.github.com>
2019-09-01 16:08:02 +1000

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -5,8 +5,8 @@ yum update
 yum install java-1.8.0-openjdk -y
 var=$(readlink -f $(which java))
 export JAVA_HOME=$var
-
 sudo yum install python3 -y
-sudo yum upgrade python3
+sudo yum install puppet
 sudo yum install git-all
-sudo yum intstall wget zip unzip
+sudo yum install wget
+sudo yum install zip
