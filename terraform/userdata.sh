f2e9cc3137e718114a24240d8e695d11c48aa5cc
VinodBansal <54194786+VinodBansal@users.noreply.github.com>
2019-09-02 14:41:30 +1000

    Update userdata.sh

--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -2,7 +2,7 @@
 # output log of userdata to /var/log/user-data.log
 cd /home/
 log=/home/boot_log.txt
-echo "Install Java /n"
+echo "Install Java " >>$log
 sudo yum install java-1.8.0-openjdk -y 
 echo "java is installed" >>$log
 echo "Set Java env variables /n" >>$log
@@ -14,26 +14,30 @@ export PATH=/opt/puppetlabs/puppet/bin:$PATH
 export PATH=/usr/bin/aws/bin:$PATH
 EOL
 source /etc/environment
-echo "install Python3"
+echo "install Python3" >>$log
 sudo yum install python3 -y
 sudo alternatives --set python /usr/bin/python3
 echo "Python is installed" >>$log
 echo "install puppet" >>$log
 sudo rpm -ivh https://yum.puppetlabs.com/puppet-release-el-8.noarch.rpm 
-sudo yum install puppet -y >>$log
+sudo yum install puppet -y
 source /etc/environment
 echo "Puppet is installed" >>$log
 echo "install wget, zip and git" >>$log
-sudo yum install wget -y >>$log
-sudo yum install zip -y >>$log
-sudo yum install git-all -y >>$log
-curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip" >>$log
-unzip awscli-bundle.zip >>$log
-sudo ./awscli-bundle/install -i /usr/bin/aws -b /usr/bin/aws >>$log
+sudo yum install wget -y
+sudo yum install zip -y
+sudo yum install git-all -y
+echo "wget,zip and git are installed" >>$log
+echo "install awscli tool" >>$log
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+sudo ./awscli-bundle/install -i /usr/bin/aws -b /usr/bin/aws
 source /etc/environment
+echo "awscli is installed" >>$log
 echo "**************************************************************" >>$log
 echo "Deploy tomcat and Application" >>$log
-aws s3 cp s3://acit-team1/Artifactory/webapp-runner.jar /home/app/ >>$log
-aws s3 cp s3://acit-team1/Artifactory/java-tomcat-maven-example.war /home/app/ >>$log
-java -jar /opt/app/webapp-runner.jar /opt/app/*.war >>$log >>$log
+echo "**************************************************************" >>$log
+aws s3 cp s3://acit-team1/Artifactory/webapp-runner.jar /home/app/
+aws s3 cp s3://acit-team1/Artifactory/java-tomcat-maven-example.war /home/app/
+java -jar /opt/app/webapp-runner.jar /opt/app/*.war >>$log
 
